56741ba82b1a2dbd2d808af5c34d71eec2ab0468
FrenzyMOON <daniil.tulyak@gmail.com>
2021-11-22 13:32:53 +0300

    lesson3

--- lesson3/lesson3_1.py
+++ lesson3/lesson3_1.py
@@ -1,11 +1,10 @@
 def calculat(a, b):
-    print("Введите 2 числа: ")
-    a, b = (int(input()), int(input()))
     if b != 0:
         return a / b
     else:
         return "Error"
 
 
-print(calculat(0, 0))
-print(calculat(0, 0))
+print("Введите 2 числа: ")
+print(calculat(int(input()), int(input())))
+print(calculat(int(input()), int(input())))
